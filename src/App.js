f62b9275118dc5363bf2fad1b560a5027058af09
A8E1 <98847922+A8E1@users.noreply.github.com>
2024-12-31 10:36:28 -0600

    moving state up to a game component that will manage history of board

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ function Square({value, onSquareClick}){
     )
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true)
-
-  const [squares, setSquares] = useState(Array(9).fill(null))
+function Board( {xIsNext, squares, onPlay} ) {
 
   function calculateWinner(squares){
     const lines = [
@@ -85,3 +82,24 @@ export default function Board() {
     )
 }
   
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currSquares = history[history.length - 1]
+  function handlePlay(){
+
+  }
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currSquares} onPlay={handlePlay}/>
+      </div>
+    <div className='game-info'>
+      <ol>
+        {/*TODO*/}
+      </ol>
+    </div>
+    </div>
+  )
+}
